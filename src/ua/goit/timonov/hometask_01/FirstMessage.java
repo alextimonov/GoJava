0476cd3852637660d3e79e37cfb87444c73ac757
Alex Timonov <timalex1206@gmail.com>
2016-02-29 16:38:23 +0200

    hometasks 1 & 2

---
+++ src/ua/goit/timonov/hometask_01/FirstMessage.java
@@ -0,0 +1,8 @@
+package ua.goit.timonov.hometask_01;
+
+public class FirstMessage {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+        System.out.println("Wh");
+    }
+}

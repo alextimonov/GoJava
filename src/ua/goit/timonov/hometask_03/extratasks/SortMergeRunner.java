52685eed22eedfc99311674da9905b79650164ca
Alex Timonov <timalex1206@gmail.com>
2016-03-04 00:57:21 +0200

    add some code to SortMerge & NumberUtils

--- src/ua/goit/timonov/hometask_03/extratasks/SortMergeRunner.java
+++ src/ua/goit/timonov/hometask_03/extratasks/SortMergeRunner.java
@@ -1,8 +1,76 @@
 package ua.goit.timonov.hometask_03.extratasks;
 
+import ua.goit.timonov.utils.NumberUtils;
+import java.util.Scanner;
+
 /**
  * Created by Alex on 03.03.2016.
  */
-public class SortMergeRunner {
 
+public class SortMergeRunner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input number of elements in Array. ");
+        int nElements = NumberUtils.inputInt(sc);
+        System.out.print("Input max value in Array. ");
+        int max = NumberUtils.inputInt(sc);
+        int[] array = NumberUtils.generateRandomArray(nElements, max);
+        System.out.println("Current array is: ");
+        printArray(array);
+        sortMerge(array, 0, array.length-1);
+        System.out.println("Sorted array is: ");
+        printArray(array);
+    }
+
+    private static int[] copy(int[] array) {
+        int[] resultArray = new int[array.length];
+        for (int i = 0; i < array.length ; i++) {
+            resultArray[i] = array[i];
+        }
+        return resultArray;
+    }
+
+    private static void sortMerge(int[] array, int leftBound, int rightBound) {
+        if (leftBound == rightBound) {
+            return;
+        }
+        else {
+            int middle = (rightBound + leftBound) / 2;
+            sortMerge(array, leftBound, middle);
+            sortMerge(array, middle+1, rightBound);
+            mergeArrays(array, leftBound, middle+1, rightBound);
+        }
+    }
+
+    private static void mergeArrays(int[] array, int leftPtr, int rightPtr, int rightBound) {
+        int nElements = rightBound - leftPtr + 1;
+        int leftStart = leftPtr;
+        int[] work = new int [nElements];
+        int leftBound = rightPtr-1;
+        int i = 0;
+        while (leftPtr <= leftBound && rightPtr <= rightBound) {
+            if (array[leftPtr] < array[rightPtr]) {
+                work[i++] = array[leftPtr++];
+            }
+            else {
+                work[i++] = array[rightPtr++];
+            }
+        }
+        while (leftPtr <= leftBound) {
+            work[i++] = array [leftPtr++];
+        }
+        while (rightPtr <= rightBound) {
+            work[i++] = array [rightPtr++];
+        }
+        for (i = 0; i < nElements; i++) {
+            array[leftStart+i] = work[i];
+        }
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length ; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 }
--- src/ua/goit/timonov/utils/NumberUtils.java
+++ src/ua/goit/timonov/utils/NumberUtils.java
@@ -1,7 +1,38 @@
 package ua.goit.timonov.utils;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by Alex on 02.03.2016.
  */
 public class NumberUtils {
+
+    public NumberUtils() {}
+
+    public static int inputInt(Scanner sc) {
+        System.out.println("Please, input integer number: ");
+        int result = 0;
+        boolean isInputSuccessful = false;
+        while (!isInputSuccessful) {
+            try {
+                String s = sc.nextLine();
+                result = Integer.parseInt(s);
+                isInputSuccessful = true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Must be integer number! Input one more: ");
+            }
+        }
+        return result;
+    }
+
+    public static int[] generateRandomArray(int nElements, int max) {
+        int[] array = new int[nElements];
+        Random rand = new Random();
+        for (int i = 0; i < nElements; i++) {
+            array[i] = rand.nextInt(max);
+        }
+        return array;
+    }
 }
